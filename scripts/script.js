c2631d32bf211289658e1375fe094aba47f82350
Roman Pigunov <jerome.malone@yandex.ru>
2026-06-23 07:46:53 +0300

    1

--- scripts/script.js
+++ scripts/script.js
@@ -12,14 +12,13 @@ import {
   getImagePath,
   debounce,
 } from "https://shoneal.github.io/rollingstone/scripts/utils.js";
+import { renderLastArticlesAndDate } from "https://shoneal.github.io/rollingstone/scripts/last-articles.js";
 import {
   initBodyElements,
   getSectionContext,
-  createImage,
   createImageBlock,
+  initializePageTop,
   renderGallery,
-  renderLastArticlesAndDate,
-  initApp,
 } from "https://shoneal.github.io/rollingstone/scripts/utils-for-cover-stories.js";
 
 const bodyElements = initBodyElements(); // Элементы тела страницы
@@ -33,30 +32,38 @@ const { basicLink, currentData } = getSectionContext(
 ); // Главная ссылка, данные по имени секции и длина объекта
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.classList.add(section); // Название сайта как класс в body
   changingTheme(); // Смена темы
-  switchingStickinessHeader(bodyElements.cardMarker, bodyElements.header); // Липкий выезжающий header
+  bodyElements.authorName.textContent = bodyElements.author.content; // Имя автора в HTML
+  switchingStickinessHeader(bodyElements.pageTopMarker, bodyElements.header); // Липкий выезжающий header
 
-  createImage(
+  initializePageTop(
     getImagePath,
     showImage,
-    bodyElements.cardImage,
     basicLink,
-    "card",
     personName,
-    true,
-  ); // Создание картинки в шапке
+    currentData,
+    bodyElements.pageTop,
+  ); // Создание видео/картинки в шапке
 
   renderGallery(
     getImagePath,
     showImage,
-    currentData,
-    bodyElements.content,
     basicLink,
     personName,
+    currentData,
+    bodyElements.content,
   ); // Вывод элементов в структуру HTML
 
-  initApp(bodyElements, renderLastArticlesAndDate, coversLinks, listsLinks); // Общая для всех инициализация
+  renderLastArticlesAndDate(
+    coversLinks,
+    listsLinks,
+    bodyElements.url,
+    4,
+    "article-content figure",
+    "first",
+    bodyElements.time,
+    data,
+  ); // Добавление последних ссылок автора и времени публикации страницы
 }); // Изначальная инициализация
 let ticking = false; // Задержка для скролла
 window.addEventListener("scroll", () => {
